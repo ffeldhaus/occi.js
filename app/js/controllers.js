f4e0f42a1924a58ab82cdfbc02c39f8b3121f477
Florian Feldhaus <florian.feldhaus@gmail.com>
2014-01-26 14:45:26 +0100

    added OCCI Core objects and OCCI Collection to parse the JSON to OCCI objects

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,7 @@ angular.module('test.controllers', []).
     controller('TestCtrl', function ($scope, $occi) {
         $scope.getCategories = $occi.getCategories()
             .success(function (categories) {
-                $scope.categories = categories;
+                $scope.categories = new Collection(categories);
             })
             .error(function (error) {
                 $scope.status = 'Unable to load data: ' + error.message;
--- app/lib/occi/occi.js
+++ app/lib/occi/occi.js
@@ -10,9 +10,116 @@ angular.module('occi', []).
         var occiFactory = {};
 
         occiFactory.getCategories = function () {
-            return $http.get(urlBase);
+            collection = $http.get(urlBase);
+            return collection;
         };
 
         return occiFactory;
 
     }]);
+
+function Attributes(attributes) {
+    for (var attribute in attributes) {
+        if (attributes.hasOwnProperty(attribute)) {
+            if (attributes[attribute] != null && typeof attributes[attribute] === 'object') {
+                this[attribute] = new Attributes(attributes[attribute]);
+            } else {
+                this[attribute] = attributes[attribute];
+            }
+        }
+    }
+}
+
+function Category(category) {
+    if (!(this instanceof Category))
+        return new Category(category)
+    this.scheme = category.scheme;
+    this.term = category.term;
+    this.title = category.title;
+    this.attributes = new Attributes(category.attributes);
+}
+
+function Kind(kind) {
+    if (!(this instanceof Kind))
+        return new Kind(kind)
+    Category.call(this, kind);
+    this.parent = kind.parent;
+    this.actions = kind.actions;
+    this.location = kind.location;
+}
+
+Kind.prototype = Object.create(Category.prototype);
+Kind.prototype.constructor = Kind;
+
+function Mixin(mixin) {
+    if (!(this instanceof Mixin))
+        return new Mixin(mixin)
+    Category.call(this, mixin);
+    this.parent = mixin.parent;
+    this.actions = mixin.actions;
+    this.location = mixin.location;
+}
+
+Mixin.prototype = Object.create(Category.prototype);
+Mixin.prototype.constructor = Mixin;
+
+function Action(action) {
+    if (!(this instanceof Action))
+        return new Action(action)
+    Category.call(this, action);
+}
+
+Mixin.prototype = Object.create(Category.prototype);
+Mixin.prototype.constructor = Mixin;
+
+function Entity(entity) {
+    this.kind = entity.kind;
+    this.mixins = entity.mixins;
+    this.attributes = new Attributes(entity.attributes);
+    this.actions = entity.actions;
+    this.location = entity.location;
+}
+
+function Resource(resource) {
+    Entity.call(this, resource);
+    this.links = resource.links;
+}
+
+Resource.prototype = Object.create(Entity.prototype);
+Resource.prototype.constructor = Resource;
+
+function Link(link) {
+    Entity.call(this, link);
+    this.rel = link.rel;
+    this.target = link.target;
+    this.source = link.source;
+}
+
+Link.prototype = Object.create(Entity.prototype);
+Link.prototype.constructor = Link;
+
+function Collection(collection) {
+    for (entry in collection) {
+        if (collection.hasOwnProperty(entry)) {
+            switch (entry) {
+                case "kinds":
+                    this.kinds = collection.kinds.map(Kind);
+                    break;
+                case "mixins":
+                    this.mixins = collection.mixins.map(Mixin);
+                    break;
+                case "actions":
+                    this.actions = collection.actions.map(Action);
+                    break;
+                case "resources":
+                    this.resources = collection.resources.map(Resource);
+                    break;
+                case "links":
+                    this.links = collection.links.map(Link);
+                    break;
+            }
+        }
+    }
+}
+
+json = '{"kinds":[{}],"mixins":[{}],"actions":[{}]}';
